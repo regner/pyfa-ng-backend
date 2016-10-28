5d8a1d78a46a3a84c9d2d789d9e1dec8fe45eb58
Regner Blok-Andersen <shadowdf@gmail.com>
2016-10-28 13:45:45 -0700

    Config values now come from environment variables with good defaults.
    Fixes #7

--- pyfa_ng_backend/config.py
+++ pyfa_ng_backend/config.py
@@ -6,8 +6,10 @@ import os
 class AppConfig(object):
     """Config object for the app that pulls from the environment."""
     DEBUG = True
-    CACHE_DEFAULT_TIMEOUT = 600
-    CACHE_TYPE = 'simple'
+    CACHE_DEFAULT_TIMEOUT = os.environ.get('CACHE_DEFAULT_TIMEOUT', 600)
+    CACHE_TYPE = os.environ.get('CACHE_TYPE', 'simple')
     SQLALCHEMY_BINDS = {
-        'sde': 'sqlite:///../eve_data/static_data/tq.db',
+        'sde': os.environ.get('SQLALCHEMY_SDE', 'sqlite:///../eve_data/static_data/tq.db'),
     }
+    EOS_JSON_DATA = os.environ.get('EOS_JSON_DATA', 'eve_data/phobos_dump/')
+    EOS_CACHE = os.environ.get('EOS_CACHE', 'eve_data/pyfa_cache/eos_tq.json.bz2')
--- pyfa_ng_backend/extensions.py
+++ pyfa_ng_backend/extensions.py
@@ -11,13 +11,16 @@ db = SQLAlchemy()
 migrate = Migrate()
 cache = Cache()
 
-data_handler = JsonDataHandler('eve_data/phobos_dump/')
-cache_handler = JsonCacheHandler('eve_data/pyfa_cache/eos_tq.json.bz2')
-SourceManager.add('tq', data_handler, cache_handler, make_default=True)
-
 
 def configure_extensions(app):
     """Registers all relevant extensions."""
     db.init_app(app)
     migrate.init_app(app, db)
     cache.init_app(app)
+
+    # Eos isn't a proper Flask extension at this time but it still needs app config to but initialized.
+    # If we decide we want to support multiple sources or share this with other Flask applications
+    # we should consider turning this into an extension.
+    data_handler = JsonDataHandler(app.config['EOS_JSON_DATA'])
+    cache_handler = JsonCacheHandler(app.config['EOS_CACHE'])
+    SourceManager.add('tq', data_handler, cache_handler, make_default=True)
